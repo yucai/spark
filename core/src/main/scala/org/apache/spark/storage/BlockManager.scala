c737df87999a699862b481f69d5c9635e55f183e
yucai <yyu1@ebay.com>
2018-08-07 13:24:16 +0800

    shuffle read opt: one disk IO in server side

--- core/src/main/scala/org/apache/spark/storage/BlockManager.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockManager.scala
@@ -392,6 +392,10 @@ private[spark] class BlockManager(
     }
   }
 
+  override def getBlockData(blockId: ContinuousShuffleBlockId): Seq[ManagedBuffer] = {
+    shuffleManager.shuffleBlockResolver.getBlockData(blockId)
+  }
+
   /**
    * Put the block locally, using the given storage level.
    *
